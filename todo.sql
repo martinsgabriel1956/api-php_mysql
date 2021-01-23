c0d1c869a62cebdca346ddc99612dbbbb244840e
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-01-23 09:35:49 -0300

    Added e Updated API documentation

--- todo.sql
+++ todo.sql
@@ -11,3 +11,14 @@ CREATE TABLE todo_list (
   priority varchar(127),
   status ENUM('ativo', 'inativo') DEFAULT 'ativo'
 );
+
+INSERT INTO todo_list 
+    (date, description, priority) 
+VALUES
+    ('2021-01-21 07:25:00', 'Fazer café da manhã.', 'alta'),
+    ('2021-01-22 12:30:00', 'Servir almoço da família.', 'alta'),
+    ('2021-01-22 20:45:00', 'Lavar os pratos do jantar.', 'média'),
+    ('2021-01-23 07:00:00', 'Acordar para um novo dia.', 'alta'),
+    ('2021-01-23 09:15:00', 'Comprar HQ do Batman.', 'baixa'),
+    ('2021-01-23 09:00:00', 'Dar aula para progrmamador Web.', 'alta')
+;
